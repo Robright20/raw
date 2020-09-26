828d3e7ea2ca207fdc3dd419db4a5c455bdb3a3c
Robert Bright <robright28@gmail.com>
2020-09-26 21:59:20 +0100

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR=./.spec
+FARMS=$DIR/*.farm
+RUN=./lem-in
+
+for farm in $FARMS; do $RUN < $farm | grep -E '^L' > $DIR/$farm.res; done
+ 
+for farm in $FARMS 
+do
+	diff $DIR/$farm.res $DIR/$farm.out 
+done
+
+
